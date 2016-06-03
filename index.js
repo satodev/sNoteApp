e99d54f6331c1a0c7084828da604ad0a3f3abcb8
satodev <hemmi.satoru@gmail.com>
2016-06-03 10:41:03 +0200

    port rectification in index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 8080;
 app.get('/', function(req,res){
 	res.send('Hello');
 	res.end();
 });
-app.listen(8080, function(){
+app.listen(port, function(){
 	console.log('server listenning to port 8080');
 });
