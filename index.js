9288f153f969af5171863d578727fcc46a73b3ba
satodev <hemmi.satoru@gmail.com>
2016-06-03 10:44:17 +0200

    add routing to sato

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 8080;
-app.get('/', function(req,res){
+const express = require('express');
+let app = express();
+let port = process.env.PORT || 8080;
+app.get('/',(req,res)=>{
 	res.send('Hello');
 	res.end();
 });
-app.listen(port, function(){
+app.get('/sato', (req, res)=>{
+	res.send('Sato page');
+	res.end();
+});
+app.listen(port,()=>{
 	console.log('server listenning to port 8080');
 });
