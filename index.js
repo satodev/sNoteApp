2e763654eba1b3520145ff0f6ec13d70e0cf08b0
satodev <hemmi.satoru@gmail.com>
2016-06-03 10:48:51 +0200

    ES5

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express');
-let app = express();
-let port = process.env.PORT || 8080;
-app.get('/',(req,res)=>{
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+app.get('/', function(req,res){
 	res.send('Hello');
 	res.end();
 });
-app.get('/sato', (req, res)=>{
+app.get('/sato', function(req, res){
 	res.send('Sato page');
 	res.end();
 });
-app.listen(port,()=>{
+app.listen(port,function(){
 	console.log('server listenning to port 8080');
 });
